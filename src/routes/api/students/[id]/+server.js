5f67b718b088167bb0c28e3494b415d98f651d49
elweday <elweday22@gmail.com>
2023-09-24 14:21:16 +0300

    add grades to the table via api

--- src/routes/api/students/[id]/+server.js
+++ src/routes/api/students/[id]/+server.js
@@ -1,6 +1,21 @@
 import { json, error } from "@sveltejs/kit";
 import db from "$lib/prisma";
 
+const gradingSys = {
+	90: "A+",
+	85: "A",
+	80: "B+",
+	75: "B",
+	70: "C+",
+	65: "C",
+	60: "D+",
+	55: "D",
+	50: "F",
+}
+const getNearest5 = (num) => {
+	return Math.round(num / 5) * 5;
+}
+
 export async function GET(requestEvent) {
 	const { params } = requestEvent;
 	const { id } = params;
@@ -43,6 +58,13 @@ export async function GET(requestEvent) {
 			mark: enrollment.grades.reduce((a, b) => a + b.mark, 0),
 			total: enrollment.instance.course.total,
 			status: (enrollment.grades.reduce((a, b) => a + b.mark, 0) / enrollment.instance.course.total)>0.5?"Passed":"Failed",
+			grade: (()=>{
+				const mark = enrollment.grades.reduce((a, b) => a + b.mark, 0)
+				const total = enrollment.instance.course.total
+				const nearest5 = getNearest5((100*mark / total).toFixed(0))
+				return nearest5 in gradingSys?gradingSys[nearest5]:"F"
+			})(),
+
 		})),
 	});
 }
