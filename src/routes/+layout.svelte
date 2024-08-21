8bea7904c20a50b0bc817bba577cf1c41ce47ad9
elweday1 <mohammednh2864@gmail.com>
2024-08-21 23:50:38 +0300

    local storage persist theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script>
 	import "../app.postcss";
+	import { beforeUpdate } from "svelte";
+	beforeUpdate(() => {
+		let body = document.querySelector('body')
+		body.setAttribute("data-theme", localStorage.getItem("theme") || "the-theme");
+		const styles = getComputedStyle(body);
+		const primaryColor = styles.getPropertyValue("--color-primary-500")
+		localStorage.setItem("primaryColor", primaryColor)
+	})
+
 </script>
+
 
 <slot/>
 
